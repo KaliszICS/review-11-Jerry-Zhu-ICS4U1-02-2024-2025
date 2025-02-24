2c53397315931341818318c687be811bf93869d4
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-02-24 15:07:50 +0000

    finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,39 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
-
+public static int[] createIntArray(){
+	int[] intArr = new int[5];
+	for (int i = 0; i < intArr.length; i++){
+		intArr[i] = i + 1;
+	}
+	return intArr;
 }
+public static String[] createArray (String word, String word2, String word3, String word4){
+	String strArr[] = {word, word2, word3, word4};
+	return strArr;
+	}
+
+public static int findValue(int num,int[] intArr){
+	for (int i =0; i< intArr.length; i++){
+		if (intArr[i] == num){
+			return i;
+		}
+	}
+	return -1;
+}
+
+public static int findThirdValue(String word, String[] strArr){
+	int counter = 0;
+	for(int i = 0; i < strArr.length; i++){
+if (word.equals(strArr[i])){
+	counter++;
+}
+if (counter == 3){
+	return i;
+}
+	}
+	return -1;
+}
+}
+
+
